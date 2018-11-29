4316f1fbee23f2c66f590387d32fecc2c2280f04
anonymous <anonymous>
2018-11-29 18:00:51 -0500

    Enable toggling of character list

--- client/src/components/UniqueCharacters.js
+++ client/src/components/UniqueCharacters.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 
 export default class UniqueCharacters extends Component {
-  render() {
-    return (
-      <div>
-        <Button size='big'>Calculate Unique Characters</Button>
-      </div>
-    )
-  }
+    state = {
+        showList: false
+    }
+
+    handleClick = () => {
+        this.toggleList()
+    }
+
+    toggleList = () => {
+        this.setState({ showList: !this.state.showList })
+    }
+
+    render() {
+        return (
+            <div>
+                <Button size='big' onClick={this.handleClick}>
+                    {this.state.showList ?
+                        "Hide Unique Characters"
+                        :
+                        "Calculate Unique Characters"}
+                </Button>
+                {this.state.showList ?
+                    "list"
+                    :
+                    null}
+            </div>
+        )
+    }
 }
