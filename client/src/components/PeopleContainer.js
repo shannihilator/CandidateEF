83837061b9f516674f574d62540c4f19c01a6dc2
anonymous <anonymous>
2018-11-29 17:56:12 -0500

    Render UniqueCharacters component

--- client/src/components/PeopleContainer.js
+++ client/src/components/PeopleContainer.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import PeopleList from './PeopleList';
+import UniqueCharacters from './UniqueCharacters';
 
 export default class PeopleContainer extends Component {
     state = {
         apiPeople: []
     }
-    
-    
-        getPeopleData = async () => {
-            const response = await axios.get('/api/people')
-            this.setState({ apiPeople: response.data.data })
-        }
-    
-        componentDidMount = () => {
-            this.getPeopleData()
-        }
+
+
+    getPeopleData = async () => {
+        const response = await axios.get('/api/people')
+        this.setState({ apiPeople: response.data.data })
+    }
+
+    componentDidMount = () => {
+        this.getPeopleData()
+    }
 
     render() {
         return (
             <div>
+                <h2>Information</h2>
+                <UniqueCharacters
+                    apiPeople={this.state.apiPeople} />
                 <h2>All People</h2>
                 <PeopleList
-                apiPeople={this.state.apiPeople} />
+                    apiPeople={this.state.apiPeople} />
             </div>
         )
     }
---
+++ client/src/components/UniqueCharacters.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default class UniqueCharacters extends Component {
+  render() {
+    return (
+      <div>
+        <Button size='big'>Calculate Unique Characters</Button>
+      </div>
+    )
+  }
+}
