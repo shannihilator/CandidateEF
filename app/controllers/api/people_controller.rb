555cb9486d2b71c24e95dba7984236163af29e9b
anonymous <anonymous>
2018-11-29 15:48:24 -0500

    Enable access to people api

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -2,6 +2,15 @@ class Api::PeopleController < ApplicationController
     include HTTParty
     
     def index
-        
+        headers = {
+            "Authorization" => "Bearer #{Figaro.env.sl_api_key}",
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        }
+
+        url = "https://api.salesloft.com/v2/people.json"
+        @people = HTTParty.get(url, :headers => headers)
+        render json: @people
     end
+
 end
