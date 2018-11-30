b2cac573296f5b744559c0f2866851dadde8e687
anonymous <anonymous>
2018-11-30 00:19:12 -0500

    Include PeopleContainer sorting test

---
+++ client/src/components/PeopleContainer.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import PeopleContainer from './PeopleContainer'
+
+describe('PeopleContainer component', () => {
+    let wrapper
+    beforeEach(() => {
+        wrapper = shallow(<PeopleContainer />)
+    })
+
+    test('Character Arrays sorted by frequency from highest to lowest', () => {
+        wrapper.setState({ characterArrays: [["a","a","a"], ["e","e","e","e","e"]] })
+        wrapper.instance().sortByFrequency()
+        expect(wrapper.state().characterArrays[0][0]).toEqual("e")
+    })
+})
