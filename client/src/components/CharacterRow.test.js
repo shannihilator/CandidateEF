63aa93eba22468d74e95e3214a712a7f61d9845e
anonymous <anonymous>
2018-11-29 22:47:37 -0500

    Include CharacterRow test

---
+++ client/src/components/CharacterRow.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import CharacterRow from './CharacterRow'
+
+describe('CharacterRow component', () => {
+    let component
+    beforeEach(() => {
+        component = shallow(<CharacterRow char={"Z"} length={10} />)
+    })
+    
+    it('should render correct character', () => {
+        expect(component.contains('Z')).toBeTruthy()
+    })
+
+    it('should render correct length', () => {
+        expect(component.contains(10)).toBeTruthy()
+    })
+})
